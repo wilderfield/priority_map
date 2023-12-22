48ecab98b3ef9b84ef52d06b0a0de9578d77b497
Bryan Lozano <b.lozano.havoc@gmail.com>
2023-12-22 00:55:03 -0800

    Add a few more tests

--- tests/priority_map_tests.cpp
+++ tests/priority_map_tests.cpp
@@ -21,6 +21,31 @@ TEST_CASE("PriorityMap operations are tested", "[priority_map]") {
         auto it = pmap.begin();
         REQUIRE(pmap.begin() == pmap.end());
     }
+    
+    // Test case for insertion and deletion
+    SECTION("Increment thousand times works correctly") {
+        for (size_t i = 0; i < 1000; i++) {
+            ++pmap[7]; // Increment value for key 7
+        }
+        auto val = pmap.begin();
+        REQUIRE(val->first == 7);  // Check key is 7
+        REQUIRE(val->second == 1000); // Check value is 1000 after increment
+    }
+    
+    // Test case for insertion and deletion
+    SECTION("Increment thousand times works correctly for two keys") {
+        for (size_t i = 0; i < 1000; i++) {
+            ++pmap[7]; // Increment value for key 7
+        }
+        for (size_t i = 0; i < 1000; i++) {
+            ++pmap[8]; // Increment value for key 8
+        }
+        auto val = pmap.begin();
+        REQUIRE(val->second == 1000); // Check value is 1000 after increment
+        ++val;
+        REQUIRE(val->second == 1000); // Check value is 1000 after increment
+
+    }
 
     // ... Additional test cases for increment, decrement, and other functionalities
 }
