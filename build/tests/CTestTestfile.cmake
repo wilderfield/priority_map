5f8b13f0117340f912da0462e409227d406cd19c
Bryan Lozano <b.lozano.havoc@gmail.com>
2023-12-27 15:43:57 -0800

    Remove unnecessary files

--- build/tests/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/bryanloz/FrequencyMap/priority_map/priority_map/tests
-# Build directory: /Users/bryanloz/FrequencyMap/priority_map/priority_map/build/tests
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/bryanloz/FrequencyMap/priority_map/priority_map/build/tests/priority_map_test_include-b12d07c.cmake")
--- build/tests/priority_map_test_tests-b12d07c.cmake
+++
@@ -1,3 +0,0 @@
-add_test( [==[PriorityMap operations are tested]==] /Users/bryanloz/FrequencyMap/priority_map/priority_map/build/tests/priority_map_test [==[PriorityMap operations are tested]==]  )
-set_tests_properties( [==[PriorityMap operations are tested]==] PROPERTIES WORKING_DIRECTORY /Users/bryanloz/FrequencyMap/priority_map/priority_map/build/tests)
-set( priority_map_test_TESTS [==[PriorityMap operations are tested]==])
